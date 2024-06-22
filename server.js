9de14e79c905f8d51d0c453afaafd266fe33f26a
WilbertFE <wilbertbernadi699@gmail.com>
2024-06-22 11:46:47 +0700

    tes7

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
 app.use(passport.initialize());
 app.use(passport.session());
 
