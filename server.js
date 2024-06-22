d2538ad6357e397fe7c16cffa77df6a6a82564cf
WilbertFE <wilbertbernadi699@gmail.com>
2024-06-22 17:12:46 +0700

    Fixing callbackURL

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://type-blog-backend.vercel.app/auth/google/callback",
       scope: ["profile", "email"],
     },
     async (request, accessToken, refreshToken, profile, done) => {
